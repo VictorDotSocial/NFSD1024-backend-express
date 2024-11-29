c2c66c24c87dcd2bc380a8752090b6af10c7b956
Daniel Herrero Hernando <danielherrerohernando@gmail.com>
2024-11-29 21:35:35 +0100

    feat: adding routes

--- backend-express/index.js
+++ backend-express/index.js
@@ -1,11 +1,27 @@
 // Importo Express
 const express = require("express");
+const crypto = require('crypto');
 
 // Instanciamos Express
 const app = express();
 
 // Para que el servidor interprete los JSON
 app.use(express.json());
+
+
+const taskList = []
+
+const foldersList = [ 
+  {  
+    id: 1,
+    name: "Tareas",
+  },
+  {
+    id: 2,
+    name: "Trabajo",
+  }
+]
+
 
 // Definimos Endpoints
 app.get("/", (req, res) => {
@@ -14,18 +30,93 @@ app.get("/", (req, res) => {
   res.send("Petición correcta. Has llamado a /, que es la raíz");
 });
 
-app.post("/register", (req, res) => {
-  console.log("El usuario ha hecho un petición al endpoint /register");
+app.get("/tasks", (req, res) => {
+  console.log("El usuario ha hecho un petición al endpoint /tasks");
+
+  console.log("QUERY", req.query);
+  if (req.query.size) {
+    const filteredTaskList = taskList.filter((task) => task.size === req.query.size);
+    res.json(filteredTaskList);
+    return;
+  }
+
+  res.json(taskList);
+});
+
+app.post("/tasks", (req, res) => {
+  console.log("El usuario ha hecho un petición al endpoint /tasks");
 
   console.log("BODY", req.body);
-  const { name, age, city } = req.body;
-  // const name = req.body.name
-  // const age = req.body.age
 
-  // res.send(`Usuario ${name} registrado correctamente`);
+  // esta es la forma pro
+  // const { name, description, age, price } = req.body;
+  // esta es la forma no pro
+  const name = req.body.name;
+  const size = req.body.size;
+
+  const newTask = { 
+    id: crypto.randomUUID(), 
+    name,
+    size,
+    done: false 
+  };
+
+  taskList.push(newTask);
+
   res.json({
-    success: true,
-    message: `Usuario ${name} registrado correctamente`,
+    message: `Tarea con name: ${name} registrada correctamente`,
+    payload: newTask,
+  });
+});
+
+app.delete("/tasks/:iddelatareaaborrar", (req, res) => {
+  const id = req.params.iddelatareaaborrar;
+
+  // ahora nos toca filtrar la lista de tareas quitando la que coincida con el id que nos han pasado
+  // taskList = taskList.filter((task) => task.id !== id);
+  const taskIndex = taskList.findIndex((task) => task.id === id);
+  if (taskIndex === -1) {
+    res.status(404).json({
+      message: `No se ha encontrado ninguna tarea con id: ${id}`,
+    });
+    return;
+  }
+  taskList.splice(taskIndex, 1);
+
+  res.json({
+    message: `Tarea con id: ${id} ha sido borrada correctamente`,
+  });
+});
+
+
+
+
+
+
+app.get("/folders", (req, res) => {
+  console.log("El usuario ha hecho un petición al endpoint /folders");
+
+  res.json(foldersList);
+});
+
+
+app.post("/folders", (req, res) => {
+  console.log("El usuario ha hecho un petición al endpoint /folders");
+
+  console.log("BODY", req.body);
+
+  // esta es la forma pro
+  // const { name, description, age, price } = req.body;
+  // esta es la forma no pro
+  const name = req.body.name;
+
+  const newFolder = { id: foldersList.length + 1, name };
+
+  foldersList.push(newFolder);
+
+  res.json({
+    message: `Carpeta con name: ${name} registrada correctamente`,
+    payload: newFolder,
   });
 });
 
